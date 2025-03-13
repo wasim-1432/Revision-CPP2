4c2b1402ccc799f967f59819c26ef158a5d1acff
wasim-1432 <wasim.ahmad0661@gmail.com>
2025-03-13 07:40:07 +0530

    Write a CPP program to calculate square of given number.

---
+++ wasim32.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"Enter any number\n";
+    cin>>a;
+    cout<<"Square of given number is="<<a*a;
+    cout<<endl;
+    return 0;
+}
