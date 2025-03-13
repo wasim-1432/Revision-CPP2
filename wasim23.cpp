ddbfd033c1de72f486ec2beaddb4ab81039a65fa
wasim-1432 <wasim.ahmad0661@gmail.com>
2025-03-13 07:28:42 +0530

    Write a CPP program to add two numbers.

---
+++ wasim23.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"Enter any two numbers"<<endl;
+    cin>>a>>b;
+    cout<<"Sum is="<<a+b;
+    cout<<endl;
+    return 0;
+}
