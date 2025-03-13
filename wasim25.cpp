adf6341720ec02806f7aa7210b287d61f00e1dec
wasim-1432 <wasim.ahmad0661@gmail.com>
2025-03-13 07:35:00 +0530

    Write a CPP program to calculate volume of cuboid.

---
+++ wasim25.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int l,b,h;
+    cout<<"Enter the length,breath & height"<<endl;
+    cin>>l>>b>>h;
+    cout<<"Volume of cuboid is="<<l*b*h;
+    cout<<endl;
+    return 0;
+}
