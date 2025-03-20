f6beaf6ddb0a64f171ac0aacd0bc8f1c14f4c2fa
wasim-1432 <wasim.ahmad0661@gmail.com>
2025-03-20 09:43:02 +0530

    Write a CPP program to find HCF of given two numbers.

---
+++ wasim71.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int Calculate_HCF_Of_Given_Two_Numbers(int a,int b);
+int main()
+{
+    int a,b;
+    cout<<"Enter any two numbers\n";
+    cin>>a>>b;
+    cout<<"HCF is="<<Calculate_HCF_Of_Given_Two_Numbers(a,b);
+    cout<<endl;
+    return 0;
+}
+int Calculate_HCF_Of_Given_Two_Numbers(int a,int b)
+{
+    int i=2,s=1;
+    while(i<=a || i<=b)
+    {
+        if(a%i==0 && b%i==0)
+        {
+            a=a/i;
+            b=b/i;
+            s=s*i;
+            continue;
+        }
+        else
+        {
+            i++;
+        }
+    }
+    return s;
+}
